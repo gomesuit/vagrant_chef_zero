c10a5f5b3a28a0400746c1c27b19beac425b1a92
gomesuit <gomesuit@gmail.com>
2015-07-01 19:40:27 +0900

    export

--- provision.sh
+++ provision.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 # set ssh key
-cd /root
-mkdir .ssh
-chmod 700 .ssh
-cd .ssh
-cat /vagrant/.ssh/test_id_rsa.pub >> authorized_keys
-chmod 600 authorized_keys
-cp /vagrant/.ssh/test_id_rsa ./id_rsa
-chmod 600 id_rsa
+#cd /root
+#mkdir .ssh
+#chmod 700 .ssh
+#cd .ssh
+#cat /vagrant/.ssh/test_id_rsa.pub >> authorized_keys
+#chmod 600 authorized_keys
+#cp /vagrant/.ssh/test_id_rsa ./id_rsa
+#chmod 600 id_rsa
 
 # install chef
 curl -L https://www.chef.io/chef/install.sh | bash
 
 echo 'export PATH=/opt/chef/embedded/bin:$PATH' >> /root/.bash_profile
+export PATH=/opt/chef/embedded/bin:$PATH
 
 # install chef-solo
 #/opt/chef/embedded/bin/gem install knife-solo --no-ri --no-rdoc
