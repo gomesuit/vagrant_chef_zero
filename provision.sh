27b1ac7aadf1f39e03b07b701a02431ae9547860
gomesuit <gomesuit@gmail.com>
2015-07-01 22:08:43 +0900

    test ssh

--- provision.sh
+++ provision.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # set ssh key
-#cd /root
-#mkdir .ssh
-#chmod 700 .ssh
-#cd .ssh
-#cat /vagrant/.ssh/test_id_rsa.pub >> authorized_keys
-#chmod 600 authorized_keys
-#cp /vagrant/.ssh/test_id_rsa ./id_rsa
-#chmod 600 id_rsa
+cd /root
+mkdir .ssh
+chmod 700 .ssh
+cd .ssh
+cat /vagrant/.ssh/test_id_rsa.pub >> authorized_keys
+chmod 600 authorized_keys
+cp /vagrant/.ssh/test_id_rsa ./id_rsa
+chmod 600 id_rsa
 
 # install chef
 curl -L https://www.chef.io/chef/install.sh | bash
